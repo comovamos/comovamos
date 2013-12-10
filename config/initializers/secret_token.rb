c433e1639f775d58ea0ea5d7eb26c5584d79f203
lecaros <jose.lecaros@gmail.com>
2013-12-10 12:33:30 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comovamos::Application.config.secret_token = '97e63df338acd6e3218821e7cdc7425cc6737df57e28d6df75e40965642c1cd2a95bb58a211729a2c4ff8d4b134449e00fa238f45c9c19c9c05e56d962f8e004'
